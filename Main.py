2012e295ca0f45b3b51f50e0b790a594c216caf4
maven291heavens <94922200+maven291heavens@users.noreply.github.com>
2026-05-09 19:02:18 +0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -3,20 +3,26 @@ import pgzrun
 WIDTH = 500
 HEIGHT = 500
 
-Title = "Color Changing Circles"
+TITLE = "Color Changing Circles"
 
-circles = ["redcircle,bluecircle,greencircle"]
-x = 0
-y = 0
+circles = ["redcircle", "bluecircle","greencircle"]
+x = 250
+y = 250
 
 current = 0
 circle = Actor(circles[current])
-circle.pos(x,y)
+circle.pos = (x,y)
 def draw():
     screen.clear()
     screen.fill("white")
     circle.draw()
 
-
+def on_mouse_down(pos):
+    global current 
+    circle.pos = pos
+    current += 1 # shorter way of current = current + 1 
+    if current >= len(circles):
+        current = 0 
+    circle.image = circles[current]
 
 pgzrun.go()
